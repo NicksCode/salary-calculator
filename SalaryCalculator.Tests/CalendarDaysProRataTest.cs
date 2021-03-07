ecdeab32c6d9d55813d3ae6cdc1ad0e84d61b59f
NicksCode <nicholas.vermaak@gmail.com>
2021-03-07 16:25:21 +0800

    Refactored tests into datasource tests

--- SalaryCalculator.Tests/CalendarDaysProRataTest.cs
+++ SalaryCalculator.Tests/CalendarDaysProRataTest.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SalaryCalculator.Core;
+
+namespace SalaryCalculator.Tests
+{
+    public class CalendarDaysProRataTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+        public static IEnumerable<TestCaseData> TestCaseDataSource
+        {
+            get
+            {
+                yield return new TestCaseData( new DateTime(2020,4,21), 10m/30m);
+                yield return new TestCaseData(new DateTime(2020, 4, 1), 1m);
+                yield return new TestCaseData(new DateTime(2020, 2, 29), 1m / 29m);
+                yield return new TestCaseData(new DateTime(2020, 2, 28), 2m / 29m);
+            }
+        }
+
+
+        [Test]
+        [TestCaseSource(typeof(CalendarDaysProRataTest), "TestCaseDataSource")]
+        public void ShouldCalculateCorrectProRataRate(DateTime startingDate, decimal expectedProRataRatio)
+        {
+            var calc = new CalendarDaysProRataCalculator();
+            var actualProRataRatio = calc.CalculateProRataRate(startingDate);
+            Assert.AreEqual(expectedProRataRatio, actualProRataRatio);
+        }
+    }
+}
--- SalaryCalculator.Tests/ScheduledDaysProRataTest.cs
+++ SalaryCalculator.Tests/ScheduledDaysProRataTest.cs
@@ -1,73 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using SalaryCalculator.Core;
 
 namespace SalaryCalculator.Tests
 {
-    public class CalendarDaysProRataTest
+    public class ScheduleDaysProRataTest
     {
         [SetUp]
         public void Setup()
         {
         }
 
-        [Test]
-        public void CanCalculateCalendarDaysWhenMonthHas30Days()
+
+        public static IEnumerable<TestCaseData> TestCaseDataSource
         {
-            // calendar days looks at how many days of the month they were employed.
-            // When an employee starts mid - month, and the company pays monthly, their first month’s pay must be pro-rated based on how much of the month they worked.
-            // There are 2 methods to calculate this, calendar days and scheduled days.
-            // Calendar days looks at how many days of the month they were employed.Eg.If they started on the 21st in a month with 30 days, then they were employed 10 out of the 30 days, therefore their monthly salary would be 10 / 30 of their full monthly salary.
-
-            var startingDate = new DateTime(2020,4, 21);
-            decimal expected = 10.0m / 30.0m;
-
-            var calc = new CalendarDaysProRataCalculator();
-            var actual = calc.CalculateProRataRate(startingDate);
-
-
-            Assert.AreEqual(expected, actual);
+            get
+            {
+                yield return new TestCaseData( new DateTime(2020,4,21), 8m/22m);
+                yield return new TestCaseData(new DateTime(2020, 4, 1), 1m);
+                yield return new TestCaseData(new DateTime(2020, 2, 29), 0m / 20m);
+                yield return new TestCaseData(new DateTime(2020, 2, 28), 1m / 20m);
+            }
         }
 
-        [Test]
-        public void CalendarDaysShouldCalculateProRataCorrectlyWhenStartingOnTheFirstDay() {
-            var startingDate = new DateTime(2020, 4, 1);
-            decimal expected = 1;
-
-            var calc = new CalendarDaysProRataCalculator();
-            var actual = calc.CalculateProRataRate(startingDate);
-
-            Assert.AreEqual(expected, actual);
-
-        }
 
         [Test]
-        public void CalendarDaysShouldCalculateProRataCorrectlyWhenStartingOnTheLastDay()
+        [TestCaseSource(typeof(ScheduleDaysProRataTest), "TestCaseDataSource")]
+        public void ShouldCalculateCorrectProRataRate(DateTime startingDate, decimal expectedProRataRatio)
         {
-            var startingDate = new DateTime(2020, 4, 30);
-            decimal expected = 1/30;
-
-            var calc = new CalendarDaysProRataCalculator();
-            var actual = calc.CalculateProRataRate(startingDate);
-
-            Assert.AreEqual(expected, actual);
-
+            var calc = new ScheduledDaysProRataCalculator();
+            var actualProRataRatio = calc.CalculateProRataRate(startingDate);
+            Assert.AreEqual(expectedProRataRatio, actualProRataRatio);
         }
-
-        [Test]
-        public void CalendarDaysShouldCalculateProRataCorrectlyWhenStartingLastDayFebruaryLeapYear()
-        {
-            var startingDate = new DateTime(2020, 2, 29);
-            decimal expected = 1m/29m;
-
-            var calc = new CalendarDaysProRataCalculator();
-            var actual = calc.CalculateProRataRate(startingDate);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-
-
-
     }
 }
